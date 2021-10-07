9bb7d2c8cdc3dcc5cc8f42c926814f55028b80da
Rob Sheehy <robert.sheehy@ittralee.ie>
2021-10-07 12:51:05 +0100

    Signed-off-by: Rob Sheehy <robert.sheehy@ittralee.ie>

---
+++ Clash2021/Assets/Manager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public GameObject character_prefab_template;
+    public GameObject townhall_template;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) Instantiate(character_prefab_template,
+                           new Vector3(Random.Range(-10f,10f),0, Random.Range(-10f, 10f)), Quaternion.identity);
+
+        if (Input.GetKeyDown(KeyCode.T)) Instantiate(townhall_template,
+                           new Vector3(Random.Range(-1000f, 1000f), 0, Random.Range(-1000f, 1000f)), Quaternion.identity);
+    }
+}
