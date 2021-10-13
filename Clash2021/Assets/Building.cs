15d1cc4950ddf7c33b07b32beb3c97e1b63e29ca
DarraghSweeney <darragh.m.sweeney@students.ittralee.ie>
2021-10-14 00:11:30 +0100

    Signed-off-by: DarraghSweeney <darragh.m.sweeney@students.ittralee.ie>

--- Clash2021/Assets/Building.cs
+++ Clash2021/Assets/Building.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Building : MonoBehaviour,IHealth
+public class Building  : MonoBehaviour,IHealth
 {
 
     Renderer myRenderer;
