3e2c640753a88a1b3f9acbe82dc0f48d9409986e
STUDENT Joseph Diggins <Joseph.Diggins@students.ittralee.ie>
2021-11-02 12:56:50 +0000

    Signed-off-by: STUDENT Joseph Diggins <Joseph.Diggins@students.ittralee.ie>

--- Clash2021/Assets/Scripts/Witch.cs
+++ Clash2021/Assets/Scripts/Witch.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Witch : MonoBehaviour, IHealth
+public class Witch : CharacterScript
 {
     enum Character_states { Idle, Move_to_Target, Attack, Death }
     int DPS = 100;
@@ -17,8 +17,6 @@ public class Witch : MonoBehaviour, IHealth
     Vector3 velocity;
     private float character_speed = 12f;
     public float attack_distance { get { return 12.0f; } }
-    private Manager theManager;
-    bool dead;
     
     void Start()
     {
@@ -31,62 +29,32 @@ public class Witch : MonoBehaviour, IHealth
         switch (my_state)
         {
             case Character_states.Idle:
-                if (current_target)
-                {
-                    my_state = Character_states.Move_to_Target;
-                }
-                else
-                {
-                    current_target = theManager.whats_my_target(this);
-                    if (current_target != null)
-                    {
-                        Vector3 from_me_to_target = current_target.transform.position - transform.position;
-                        velocity = character_speed * from_me_to_target.normalized;
-                        transform.LookAt(current_target.transform);
-                        my_state = Character_states.Move_to_Target;
-                    }
-                }
-
+              
                 break;
 
             case Character_states.Move_to_Target:
-                if (current_target != null)
-                    if (within_attack_range(current_target))
-                    {
-                        my_state = Character_states.Attack;
-                        attack_timer = 0;
-                        velocity = Vector3.zero;
-                    }
-                    else
-                    {
-                        my_state = Character_states.Idle;
-                    }
+                if(within_attack_range(current_target))
+                {
+                    my_state = Character_states.Attack;
+                    attack_timer = 0;
+                    velocity = Vector3.zero;
+                }
 
                 transform.position += velocity * Time.deltaTime;
                 break;
 
             case Character_states.Attack:
-
-                if (current_target)
-                    if (attack_timer <= 0f)
-                    {
-                        current_target.takeDamage((int)((float)DPS * attack_time_interval));
-                        attack_timer = attack_time_interval;
-                    }
-
-                    else
-                        my_state = Character_states.Idle;
+                if (attack_timer <= 0f)
+                {
+                    current_target.takeDamage((int)((float)DPS * attack_time_interval));
+                    attack_timer = attack_time_interval;
+                }
 
                 attack_timer -= Time.deltaTime;
 
                 break;
 
-
             case Character_states.Death:
-                if (dead == true)
-                {
-                    Destroy(gameObject);
-                }
 
                 break;
         }
@@ -98,7 +66,6 @@ public class Witch : MonoBehaviour, IHealth
             if (current_target)
             {
                 assign_target(current_target);
-
             }
         }
     }
@@ -120,10 +87,6 @@ public class Witch : MonoBehaviour, IHealth
             my_state = Character_states.Idle;
     }
 
-    internal void ImtheMan(Manager manager)
-    {
-        theManager = manager;
-    }
     private bool within_attack_range(Building current_target)
     {
         return (Vector3.Distance(transform.position, current_target.transform.position) < current_target.attack_distance);
@@ -131,13 +94,7 @@ public class Witch : MonoBehaviour, IHealth
 
     public void takeDamage(int how_much_damage)
     {
-        Debug.Log("Im being damaged");
-        CHP -= how_much_damage;
-        if (CHP <= 0)
-        {
-            dead= true;
-            Debug.Log("Dead");
-        }
+        throw new NotImplementedException();
     }
     public void repair(int v)
     {
